7d92f04c9472eb91c8d0a9c1f4faa55f0b03f1fa
ogilg <52980237+ogilg@users.noreply.github.com>
2020-09-13 17:42:39 +0100

    Added get_rate_of_return to trade class

--- model/trade.py
+++ model/trade.py
@@ -5,14 +5,19 @@ class Trade:
         self.entry_date = entry_date
         self.exit_date = exit_date
 
-    def set_entry_amount(self, entry_amount):
-        self.entry_amount = entry_amount
+    def set_entry_amount(self, entry_capital):
+        self.entry_capital = entry_capital
 
-    def get_exit_amount(self):
+    def get_exit_capital(self):
         # Use stock data to calculate
         pass
 
     def calculate_profit(self):
-        self.profit = self.get_exit_amount() - self.entry_amount
-        self.win = self.profit > 0  
+        self.profit = self.get_exit_capital() - self.entry_capital
+        self.win = self.profit > 0
+
+    def get_rate_of_return(self):
+        self.rate_of_return = (self.get_exit_capital() - self.entry_capital) / self.entry_capital
+        print(f"Your rate of return is {self.rate_of_return * 100}%")
+        return self.rate_of_return
 
