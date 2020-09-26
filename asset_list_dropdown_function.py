290fa34a52bb71221c9806d1809de969a757b2be
Torus403 <52376024+Torus403@users.noreply.github.com>
2020-09-26 15:26:27 +0100

    Asset location piechart

---
+++ asset_list_dropdown_function.py
@@ -0,0 +1,40 @@
+import yfinance as yf
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go
+from datetime import datetime as dt
+from datetime import timedelta
+from dash.dependencies import Input, Output
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+asset_list = ['MSFT', 'AAPL', 'TSLA', 'KO', 'RR.L', '^GSPC']
+#asset_name_list = []
+
+#for asset in asset_list:
+    #asset_name = ((yf.Ticker(asset)).info)['shortName']
+   #asset_name_list.append(asset_name)
+
+app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='dropdown',
+        options= [asset_list],
+        value='value',
+        multi =False
+    ),
+    html.Div(id='output-container')
+])
+
+
+@app.callback(
+    dash.dependencies.Output('output-container', 'children'),
+    [dash.dependencies.Input('dropdown', 'value')])
+def update_output(value):
+    return 'this'
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
